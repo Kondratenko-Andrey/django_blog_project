239d1f4248e9fcfe06e1fe58fb32c65b0b6d17e4
Andrey Kondratenko <andrey92kondr@gmail.com>
2024-08-05 08:59:08 +0200

    Slug URL added

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,13 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, id):
-    posts = get_object_or_404(Post, id=id,
-                              status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post': posts})
+                  {'post': post})
